c3419158cd4d979903824f3f9b5576d10969f9d5
chatsagnik <sagnik.chatterjee95@gmail.com>
2022-07-25 18:18:18 +0530

    added i3 and polybar support for external monitor

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -8,7 +8,6 @@ polybar-msg cmd quit
 
 # Launch bar1 and bar2
 echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
-polybar example 2>&1 | tee -a /tmp/polybar1.log & disown
-# polybar bar2 2>&1 | tee -a /tmp/polybar2.log & disown
-
+polybar laptop 2>&1 | tee -a /tmp/polybar1.log & disown
+polybar external 2>&1 | tee -a /tmp/polybar2.log & disown
 echo "Bars launched..."
